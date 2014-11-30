4ede5afa2df9ed21e53c9d7ed60a8d5490d2d009
Corey Edwards <tensai@zmonkey.org>
2014-11-29 17:11:41 -0700

    should be || instead of &&

--- sprite.cpp
+++ sprite.cpp
@@ -2,7 +2,7 @@
 
 bool Sprite::moving()
 {
-	return (motion.movement.x != 0) && (motion.movement.y != 0);
+	return (motion.movement.x != 0) || (motion.movement.y != 0);
 }
 
 bool Sprite::can_move(char direction)
