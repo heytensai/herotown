078fbea9b47f86dc46409f56720adc7133413f7f
Corey Edwards <tensai@zmonkey.org>
2014-12-05 17:10:21 -0700

    bit fields were wrong

--- input.h
+++ input.h
@@ -5,8 +5,8 @@
 #define INPUT_STATE_MOVE_DOWN  0x0002
 #define INPUT_STATE_MOVE_LEFT  0x0004
 #define INPUT_STATE_MOVE_RIGHT 0x0008
-#define INPUT_STATE_DROP_COIN  0x000f
-#define INPUT_STATE_QUIT       0x0010
+#define INPUT_STATE_DROP_COIN  0x0010
+#define INPUT_STATE_QUIT       0x0020
 
 typedef int input_state_t;
 
